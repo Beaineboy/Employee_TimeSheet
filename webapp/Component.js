6dd1e924fd0b7dc88830b9c0fa4e294d15e5b253
Wasim Shaikh <wasim.s@intellectbizware.com>
2024-06-11 04:47:08 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.ibspl.emplistrepo.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
